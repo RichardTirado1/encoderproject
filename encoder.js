dd5877568f27d821f6bac73b294f799f4e4a098b
Richard Tirado <richardtirado1@yahoo.com>
2016-10-12 09:06:59 -0700

    I added which radio button is checked

--- encoder.js
+++ encoder.js
@@ -1,5 +1,49 @@
-var userInput = $("#input").val();
+var letters = {
+  65:"a",
+  66:"b",
+  67:"c",
+  68:"d",
+  69:"e",
+  70:"f",
+  71:"g",
+  72:"h",
+  73:"i",
+  74:"j",
+  75:"k",
+  76:"l",
+  77:"m",
+  78:"n",
+  79:"o",
+  80:"p",
+  81:"q",
+  82:"r",
+  83:"s",
+  84:"t",
+  85:"u",
+  86:"v",
+  87:"w",
+  88:"x",
+  89:"y",
+  90:"z",
+}
 
 $('#inputArea').keydown(function(e){
-   $('#textArea').append(userInput);
+   $('#textArea').append(letters[e.keyCode]);
 });
+//Above this is echoing the input out to the text area
+
+$("input[type='radio']").click(function(e) {
+   $("input:checked").prop('checked',false);
+   $(this).prop('checked',true);
+ });
+//Part 4: Changing which radio button is checked.
+
+if ($("input:checked").val() == "echo") {
+
+}
+else ($("input:checked").val() == "Caesar cipher") {
+
+}
+else ($("input:checked").val() == "Heiroglyphics") {
+
+}
